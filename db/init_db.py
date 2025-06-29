932163473550a51ac962545efea85d70f10b9abe
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-29 11:50:22 +0300

    configure db init

--- db/init_db.py
+++ db/init_db.py
@@ -2,6 +2,10 @@ import asyncio
 from db.base import Base
 from db.session import engine
 
+# Import it here because postgres needs to know about the models before creating tables
+from db.lore_piece_db import LorePiece
+
+
 async def init_db():
     # Create all tables
     async with engine.begin() as conn:
