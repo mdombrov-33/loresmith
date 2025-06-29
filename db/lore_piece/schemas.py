29a7794145901f49394edd313c1753ac984e9145
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-29 13:47:38 +0300

    create schemas for lore_piece

---
+++ db/lore_piece/schemas.py
@@ -0,0 +1,18 @@
+from typing import Optional, Dict
+from pydantic import BaseModel
+from constants.themes import Theme
+
+
+class LorePieceCreate(BaseModel):
+    name: str
+    description: str
+    type: str
+    theme: Theme
+    details: Optional[Dict] = None
+
+
+class LorePieceRead(LorePieceCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
