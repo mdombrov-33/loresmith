ada31274e13bc542880cb787418d90599d037a7f
Maksym Dombrov <maksymdombrov@gmail.com>
2025-08-10 22:32:39 +0300

    fix: fix alembic migrations issues

--- alembic/versions/05dbf88625a6_sync_schema.py
+++ alembic/versions/05dbf88625a6_sync_schema.py
@@ -1,10 +1,11 @@
-"""initial_schema
+"""sync_schema
 
-Revision ID: 16ac0cbe3e49
-Revises: 
-Create Date: 2025-08-10 19:25:28.993178
+Revision ID: 05dbf88625a6
+Revises: 666c68d19c77
+Create Date: 2025-08-10 19:30:21.969472
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '16ac0cbe3e49'
-down_revision: Union[str, Sequence[str], None] = None
+revision: str = "05dbf88625a6"
+down_revision: Union[str, Sequence[str], None] = "666c68d19c77"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
