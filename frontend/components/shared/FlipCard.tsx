c8c7aa40205f2d89872af2dc22568fdd2e8fae8f
Maksym Dombrov <maksymdombrov@gmail.com>
2025-10-29 22:08:41 +0200

    fix: remove unused prop from flip card

--- frontend/components/shared/FlipCard.tsx
+++ frontend/components/shared/FlipCard.tsx
@@ -14,7 +14,6 @@ export default function FlipCard({
   frontContent,
   backContent,
   className = "",
-  isSelected = false,
   onClick,
 }: FlipCardProps) {
   return (
@@ -22,14 +21,14 @@ export default function FlipCard({
       className={`group perspective-1000 h-full w-full cursor-pointer ${className}`}
       onClick={onClick}
     >
-      <div className="relative h-full w-full transform-style-3d transition-transform duration-700 group-hover:rotate-y-180">
+      <div className="transform-style-3d relative h-full w-full transition-transform duration-700 group-hover:rotate-y-180">
         {/* Front Face */}
-        <div className="backface-hidden absolute inset-0 h-full w-full">
+        <div className="absolute inset-0 h-full w-full backface-hidden">
           {frontContent}
         </div>
 
         {/* Back Face */}
-        <div className="backface-hidden rotate-y-180 absolute inset-0 h-full w-full">
+        <div className="absolute inset-0 h-full w-full rotate-y-180 backface-hidden">
           {backContent}
         </div>
       </div>
